1f73386976f42681d0abf113dcf429add58d89d7
Angelo Veltens <angelo.veltens@online.de>
2013-07-22 23:44:17 +0200

    added "new meter" form

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,25 +5,25 @@
 angular.module ('meterea.controllers', []).
 
 
-    controller (
-    'OverviewCtrl', ['$scope', 'Meter', function (scope, Meter) {
+    controller ('OverviewCtrl', ['$scope', 'Meter',
+    function (scope, Meter) {
         scope.meters = Meter.query ();
+        scope.date = '2013-07-22'; // TODO: set current date
+    }]).
 
-    }])
-
-    .controller (
-    'MeterCtrl', ['$scope', '$routeParams', 'Meter', function (scope, routeParams, Meter) {
+    controller ('MeterCtrl', ['$scope', '$routeParams', 'Meter',
+    function (scope, routeParams, Meter) {
         scope.meter = Meter.get ({meterId: routeParams.meterId}, function (meter) {
         });
-    }])
-    .controller ('MainCtrl', ['$scope', '$location', function (scope, location) {
+    }]).
 
+    controller ('MainCtrl', ['$scope', '$location',
+    function (scope, location) {
+        scope.activePath = null;
+        scope.$on ('$routeChangeSuccess', function () {
+            scope.activePath = location.path ();
+            console.log (location.path ());
+        });
 
-    scope.activePath = null;
-    scope.$on ('$routeChangeSuccess', function () {
-        scope.activePath = location.path ();
-        console.log (location.path ());
-    });
-
-}]);
+    }]);
 
